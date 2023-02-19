463ba28c27c76d495738ab665c60d05c22df69e6
ColoChampre <colochampre3@gmail.com>
2023-02-19 19:43:51 -0300

    Added: RoRmod.java

---
+++ src/main/java/io/github/colochampre/riskofrainmod/RoRmod.java
@@ -0,0 +1,8 @@
+package io.github.colochampre.riskofrainmod;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(RoRmod.MODID)
+public class RoRmod {
+  public static final String MODID = "riskofrainmod";
+}
