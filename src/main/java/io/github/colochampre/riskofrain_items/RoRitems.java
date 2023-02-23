764356da98be7d8f15538e912583e9a1d3bc7f82
ColoChampre <colochampre3@gmail.com>
2023-02-22 22:01:18 -0300

    Initial commit RoR_items 1.19.3

---
+++ src/main/java/io/github/colochampre/riskofrain_items/RoRitems.java
@@ -0,0 +1,8 @@
+package io.github.colochampre.riskofrain_items;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(RoRitems.MODID)
+public class RoRitems {
+  public static final String MODID = "riskofrain_items";
+}
