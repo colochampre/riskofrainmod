cf3932f4d4af1fe0a00125e0b35e8af43ea35e33
ColoChampre <colochampre3@gmail.com>
2023-03-05 23:30:15 -0300

    Lemurian eyes layer removed

--- src/main/java/io/github/colochampre/riskofrain_mobs/client/renderer/LemurianRenderer.java
+++ src/main/java/io/github/colochampre/riskofrain_mobs/client/renderer/LemurianRenderer.java
@@ -1,42 +1,33 @@
 package io.github.colochampre.riskofrain_mobs.client.renderer;
 
-import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.blaze3d.vertex.VertexConsumer;
 import io.github.colochampre.riskofrain_mobs.RoRmod;
 import io.github.colochampre.riskofrain_mobs.client.models.LemurianModel;
-import io.github.colochampre.riskofrain_mobs.client.renderer.layers.LemurianEyesLayer;
 import io.github.colochampre.riskofrain_mobs.entities.LemurianEntity;
 import io.github.colochampre.riskofrain_mobs.events.ModClientEvents;
-import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
-import net.minecraft.client.renderer.entity.LivingEntityRenderer;
 import net.minecraft.client.renderer.entity.MobRenderer;
-import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.level.LightLayer;
 
 public class LemurianRenderer extends MobRenderer<LemurianEntity, LemurianModel<LemurianEntity>> {
   private static final ResourceLocation DEFAULT_TEXTURE = new ResourceLocation(RoRmod.MODID, "textures/entity/lemurian/lemurian_default.png");
+  //private static final ResourceLocation LEMURIAN_EYES = new ResourceLocation(RoRmod.MODID, "textures/entity/lemurian/lemurian_eyes.png");
 
   public LemurianRenderer(EntityRendererProvider.Context context) {
     super(context, new LemurianModel<>(context.bakeLayer(ModClientEvents.LEMURIAN_LAYER)), 0.4F);
-    this.addLayer(new LemurianEyesLayer(this));
+    //this.addLayer(new LemurianEyesLayer(this));
   }
-
   @Override
   protected float getFlipDegrees(LemurianEntity entity) {
-    return 0;
+    return 90;
   }
 
   @Override
   public ResourceLocation getTextureLocation(LemurianEntity entity) {
     return LemurianRenderer.DEFAULT_TEXTURE;
   }
-
+  /*
   @Override
-  public void render(LemurianEntity lemurian, float p_114209_, float partialTicks, PoseStack poseStack, MultiBufferSource bufferSource,
-                     int packedLight) {
+  public void render(LemurianEntity lemurian, float p_114209_, float partialTicks, @NotNull PoseStack poseStack, @NotNull MultiBufferSource bufferSource, int packedLight) {
     long roundedTime = lemurian.level.getDayTime() % 24000;
     boolean night = roundedTime >= 13000 && roundedTime <= 22000;
     BlockPos ratPos = lemurian.blockPosition();
@@ -49,12 +40,12 @@ public class LemurianRenderer extends MobRenderer<LemurianEntity, LemurianModel<
       brightness = Math.max(i, j);
     }
     if (brightness < 7) {
-      final ResourceLocation EYES = new ResourceLocation("textures/entity/lemurian/lemurian_eyes.png");
-      VertexConsumer ivertexbuilder = bufferSource.getBuffer(RenderType.eyes(EYES));
+      VertexConsumer ivertexbuilder = bufferSource.getBuffer(RenderType.eyes(LEMURIAN_EYES));
       this.model.renderToBuffer(poseStack, ivertexbuilder, packedLight, LivingEntityRenderer.getOverlayCoords(lemurian, 0.0F), 1.0F, 1.0F, 1.0F, 1.0F);
     }
     this.model.prepareMobModel(lemurian, 0.0F, 0.0F, partialTicks);
 
     super.render(lemurian, p_114209_, partialTicks, poseStack, bufferSource, packedLight);
   }
+  */
 }
--- src/main/java/io/github/colochampre/riskofrain_mobs/client/renderer/layers/LemurianEyesLayer.java
+++ src/main/java/io/github/colochampre/riskofrain_mobs/client/renderer/layers/LemurianEyesLayer.java
@@ -1,5 +1,6 @@
 package io.github.colochampre.riskofrain_mobs.client.renderer.layers;
 
+import io.github.colochampre.riskofrain_mobs.RoRmod;
 import io.github.colochampre.riskofrain_mobs.client.models.LemurianModel;
 import io.github.colochampre.riskofrain_mobs.entities.LemurianEntity;
 import net.minecraft.client.renderer.RenderType;
@@ -8,14 +9,14 @@ import net.minecraft.client.renderer.entity.layers.EyesLayer;
 import net.minecraft.resources.ResourceLocation;
 
 public class LemurianEyesLayer extends EyesLayer<LemurianEntity, LemurianModel<LemurianEntity>> {
-    static final RenderType EYES = RenderType.eyes(new ResourceLocation("textures/entity/lemurian/lemurian_eyes.png"));
+  static final RenderType LEMURIAN_EYES = RenderType.eyes(new ResourceLocation(RoRmod.MODID, "textures/entity/lemurian/lemurian_eyes.png"));
 
-    public LemurianEyesLayer(RenderLayerParent<LemurianEntity, LemurianModel<LemurianEntity>>layer) {
-        super(layer);
-    }
+  public LemurianEyesLayer(RenderLayerParent<LemurianEntity, LemurianModel<LemurianEntity>> layer) {
+    super(layer);
+  }
 
-    @Override
-    public RenderType renderType() {
-        return EYES;
-    }
+  @Override
+  public RenderType renderType() {
+    return LEMURIAN_EYES;
+  }
 }
